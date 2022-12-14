1dcf440ed69bb6c162ec5b0f6787fd8e5a85e4fb
scaarifintl <scaarifintl@gmail.com>
2022-12-14 09:08:05 +0300

    implement measure_height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * get_height - measures the height a tree from the given node
+ * @tree: the node to start measuring from
+ * Return: the measured height
+ */
+size_t get_height(const binary_tree_t *tree)
+{
+	size_t l_height, r_height;
+
+	/* check that tree is defined */
+	if (tree == NULL)
+		return (0);
+	/* get the sub-tree depths */
+	l_height = get_height(tree->left);
+	r_height = get_height(tree->right);
+	/* take the larger (deeper) of the two, add root node */
+	if (l_height > r_height)
+		return (l_height + 1);
+	return (r_height + 1); /* otherwise */
+}
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * NOTE: the length of path from root to deepest node
+ * @tree: pointer to the root node of the tree to be measured
+ * Return: the height measured
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (get_height(tree) - 1);
+}
